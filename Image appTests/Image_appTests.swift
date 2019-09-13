f39d7d6a03bc76e66029c54451cec0e161966826
COE-009 <coe-009@coe-009s-imac.local>
2019-09-13 15:31:43 +0530

    Initial Commit

---
+++ Image appTests/Image_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Image_appTests.swift
+//  Image appTests
+//
+//  Created by COE-009 on 13/09/19.
+//  Copyright © 2019 COE-009. All rights reserved.
+//
+
+import XCTest
+@testable import Image_app
+
+class Image_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
