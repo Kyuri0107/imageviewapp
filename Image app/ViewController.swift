dbee8974fafdc67c60a03ef3d573ef39715e123e
COE-009 <coe-009@coe-009s-imac.local>
2019-09-19 13:45:40 +0530

    This is my image app!

--- Image app/ViewController.swift
+++ Image app/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var Array = [UIImage]()
+    
+    @IBOutlet weak var Imageview: UIImageView!
+    
+    @IBOutlet weak var Slider1: UISlider!
+    
+    @IBAction func Slider(_ sender: UISlider) {
+        let value=Int(sender.value)
+        Imageview.image=Array[value]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Array=[#imageLiteral(resourceName: "k10"),#imageLiteral(resourceName: "k8"),#imageLiteral(resourceName: "k6"),#imageLiteral(resourceName: "k3"),#imageLiteral(resourceName: "k9")]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Previous(_ sender: Any) {
+        Slider1.value-=1
+        Imageview.image=Array[Int(Slider1.value)]
+    }
+    
+    @IBAction func Next(_ sender: Any) {
+        Slider1.value+=1
+        Imageview.image=Array[Int(Slider1.value)]
+    }
 }
 
